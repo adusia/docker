169808627ddc34ec313857a608fc03f70b1012ec
Ayush Dusia <adusia@udel.edu>
2015-05-05 15:50:13 -0400

    mounted net_cls cgroup and added getclassId utility

--- vendor/src/github.com/docker/libcontainer/cgroups/fs/net_cls.go
+++ vendor/src/github.com/docker/libcontainer/cgroups/fs/net_cls.go
@@ -1,22 +1,17 @@
 package fs
 
 import (
-	"bufio"
-	"os"
-	"path/filepath"
-	"strconv"
-
 	"github.com/docker/libcontainer/cgroups"
 	"github.com/docker/libcontainer/configs"
+	"github.com/docker/libcontainer/utils"	
+
 )
 
-type CpuGroup struct {
+type NetClsGroup struct {
 }
 
-func (s *CpuGroup) Apply(d *data) error {
-	// We always want to join the cpu group, to allow fair cpu scheduling
-	// on a container basis
-	dir, err := d.join("cpu")
+func (s *NetClsGroup) Apply(d *data) error {
+	dir, err := d.join("net_cls")
 	if err != nil {
 		if cgroups.IsNotFound(err) {
 			return nil
@@ -32,56 +27,18 @@ func (s *CpuGroup) Apply(d *data) error {
 	return nil
 }
 
-func (s *CpuGroup) Set(path string, cgroup *configs.Cgroup) error {
-	if cgroup.CpuShares != 0 {
-		if err := writeFile(path, "cpu.shares", strconv.FormatInt(cgroup.CpuShares, 10)); err != nil {
-			return err
-		}
+func (s *NetClsGroup) Set(path string, cgroup *configs.Cgroup) error {
+  classId := utils.GetNetClsClassId()	
+  if err := writeFile(path, "net_cls.classid", classId); err != nil {
+	  	return err
 	}
-	if cgroup.CpuPeriod != 0 {
-		if err := writeFile(path, "cpu.cfs_period_us", strconv.FormatInt(cgroup.CpuPeriod, 10)); err != nil {
-			return err
-		}
-	}
-	if cgroup.CpuQuota != 0 {
-		if err := writeFile(path, "cpu.cfs_quota_us", strconv.FormatInt(cgroup.CpuQuota, 10)); err != nil {
-			return err
-		}
-	}
-
 	return nil
 }
 
-func (s *CpuGroup) Remove(d *data) error {
-	return removePath(d.path("cpu"))
+func (s *NetClsGroup) Remove(d *data) error {
+	return removePath(d.path("devices"))
 }
 
-func (s *CpuGroup) GetStats(path string, stats *cgroups.Stats) error {
-	f, err := os.Open(filepath.Join(path, "cpu.stat"))
-	if err != nil {
-		if os.IsNotExist(err) {
-			return nil
-		}
-		return err
-	}
-	defer f.Close()
-
-	sc := bufio.NewScanner(f)
-	for sc.Scan() {
-		t, v, err := getCgroupParamKeyValue(sc.Text())
-		if err != nil {
-			return err
-		}
-		switch t {
-		case "nr_periods":
-			stats.CpuStats.ThrottlingData.Periods = v
-
-		case "nr_throttled":
-			stats.CpuStats.ThrottlingData.ThrottledPeriods = v
-
-		case "throttled_time":
-			stats.CpuStats.ThrottlingData.ThrottledTime = v
-		}
-	}
+func (s *NetClsGroup) GetStats(path string, stats *cgroups.Stats) error {
 	return nil
 }
